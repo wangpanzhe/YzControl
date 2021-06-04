443a5a09b3e187c489875c4494d0736965d86f47
王胖喆 <wangpangzhe@wangpangzhedeMacBook-Pro.local>
2021-06-04 19:26:13 +0800

    creat project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YzControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
